2b405865cede8c7c5ae1cbec95a4d3a4a8db47ec
churchillkirigwi-cmd <churchillkirigwi@gmail.com>
2026-04-27 11:21:08 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6o6lgDHFRg_gQkio9HIpcnU25DB2Ctu8",
+  authDomain: "sunzurirestaurant.firebaseapp.com",
+  projectId: "sunzurirestaurant",
+  storageBucket: "sunzurirestaurant.appspot.com",
+  messagingSenderId: "1079501361337",
+  appId: "1:1079501361337:web:8385d1c20f8d77ac804b68"
+};
+
+firebase.initializeApp(firebaseConfig);
